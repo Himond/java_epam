bcfec43ed29d148479b2759f632f66fc9c12e723
Ihar Litvinko <i.litvinko.minsk@mail.ru>
2020-10-05 16:56:44 +0300

    java_epam

---
+++ src/algorithmization/OneDimensionalArraysSecondTask.java
@@ -0,0 +1,21 @@
+package algorithmization;
+
+import java.util.Arrays;
+
+public class OneDimensionalArraysSecondTask {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int num = 7;
+        replase(arr, num);
+    }
+    public static void replase(int[] arr, int Z){
+        int repcount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > Z) {
+                arr[i] = Z;
+                repcount += 1;
+            }
+        }
+        System.out.println("Modified array: " + Arrays.toString(arr) + "\n" +  "Number of replacements: " + repcount);
+    }
+}
