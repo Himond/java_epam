2690a13b2c9cc8f83a21c947d2fd87e4b604236b
Ihar Litvinko <i.litvinko.minsk@mail.ru>
2020-10-07 16:35:30 +0300

    java_epam

---
+++ src/algorithmization/MultdimArraysEighthTask.java
@@ -0,0 +1,29 @@
+package algorithmization;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultdimArraysEighthTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int [][] multArr = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+
+        for (int[] elem : replacingColumns(multArr, n, m)) {
+            System.out.println(Arrays.toString(elem));
+        }
+    }
+
+    public static int[][] replacingColumns(int[][] arr, int n, int m){
+        for (int i = 0; i < arr.length; i++) {
+            int repl = arr[i][m];
+            arr[i][m] = arr[i][n];
+            arr[i][n] = repl;
+        }
+        return arr;
+    }
+
+
+
+}
---
+++ src/algorithmization/MultdimArraysTenthTask.java
@@ -0,0 +1,16 @@
+package algorithmization;
+
+public class MultdimArraysTenthTask {
+    public static void main(String[] args) {
+        int [][] multArr = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, -4}};
+        positiveDiag(multArr);
+    }
+    public static void positiveDiag(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i][i] > 0){
+                System.out.println("matrix element with position [" + i  + ", " + i + "]: "  + arr[i][i]);
+            }
+
+        }
+    }
+}
