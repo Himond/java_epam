cbf89763c8b3c37ccaeb5427a495a86b1ed8a8ba
Ihar Litvinko <i.litvinko.minsk@mail.ru>
2020-10-19 16:54:56 +0300

    java_epam

--- src/stringsAndBasicsOfTextProcessing/StringAsArrayFirstTask.java
+++ src/stringsAndBasicsOfTextProcessing/StringAsArrayFirstTask.java
@@ -3,22 +3,23 @@ package stringsAndBasicsOfTextProcessing;
 public class StringAsArrayFirstTask {
     public static void main(String[] args) {
         String[] str = {"variableNewA", "variableNewB", "variableNewC"};
-        String B = "AfdsfaAFF";
-        for (int i = 0; i < B.length(); i++) {
-            System.out.println(B + "C");
-        }
-        System.out.println(B);
-
-
-
-    }
-    public String[] toSnakeCase(String[] str){
+        str = toSnakeCase(str);
         for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
+        }
+    }
+    public static String[] toSnakeCase(String[] str){
+        for (int i = 0; i < str.length; i++) {
+            String newStr = "";
             for (int j = 0; j < str[i].length(); j++) {
-                if(str[i].charAt(j) > 65){
-
+                if((int)str[i].charAt(j) < 93){
+                    newStr += '_';
+                    newStr += (char)(32 + (int)str[i].charAt(j));
+                }else {
+                    newStr += str[i].charAt(j);
                 }
             }
+            str[i] = newStr;
         }
 
         return str;
