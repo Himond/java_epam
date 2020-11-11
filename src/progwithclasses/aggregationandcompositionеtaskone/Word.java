42d2d4776f472745d0a5a10895af74cf4823c2fd
Ihar Litvinko <i.litvinko.minsk@mail.ru>
2020-11-11 14:44:33 +0300

    java_epam

---
+++ src/progwithclasses/aggregationandcompositionеtaskone/Word.java
@@ -0,0 +1,40 @@
+package progwithclasses.aggregationandcompositionеtaskone;
+
+/*Создать объект класса Текст, используя классы Предложение, Слово.
+Методы: дополнить текст, вывести на консоль текст, заголовок текста.*/
+
+
+public class Word {
+
+    private String word;
+
+    public Word(String word) {
+        for (char ch: word.toCharArray()){
+            if((ch > 90  && ch < 97) || ch < 65 || ch > 122){
+                throw new IllegalArgumentException("The word is wrong");
+            }
+        }
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        for (char ch: word.toCharArray()){
+            if((ch > 90  && ch < 97) || ch < 65 || ch > 122){
+                throw new IllegalArgumentException("The word is wrong");
+            }
+        }
+        this.word = word;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "word='" + word + '\'' +
+                '}';
+    }
+
+}
